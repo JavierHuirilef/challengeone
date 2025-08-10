44fca50796f725dbaf082b0ea71db05357517ecc
Javier <JAVIERHUIRILEF1@GMAIL.COM>
2025-08-10 16:37:35 -0400

    Update app.js
    Sorteo listo!

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,18 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo = document.getElementById('amigo');
 let listaDeAmigos = [];
-let ullistaDeAmigos = document.getElementById('listaAmigos');
+let ulListaDeAmigos = document.getElementById('listaAmigos');//mostrar lista de amigos en etiqueta ul
 
 function agregarAmigo(){
     let nombreAmigo = amigo.value;
     if(nombreAmigo === ''){
-        alert("El campo nombre está vacío, porfavor ingresa un nombre");
+        alert("-debe ingresar un nombre a la lista");
         return;
     }else if(!isNaN(nombreAmigo)){
-        alert("Debes ingresar un nombre válido (sólo letras)");
+        alert("El valor ingresado es incorrecto, favor intente de nuevo");
         return;
     }else if (listaDeAmigos.includes(nombreAmigo)) { 
-        alert("Este nombre ya ha sido ingresado");
+        alert("Valor duplicado, intente otra vez");
         return;
     } else{
         listaDeAmigos.push(nombreAmigo);//agrega amigo a la lista
@@ -20,6 +20,23 @@ function agregarAmigo(){
         limpiarCampoNombre();
     }   
 };
+function sortearAmigo(){
+    if(listaDeAmigos.length=== 0){
+        alert("La lista debe contener nombres");
+        return;
+    }else if(listaDeAmigos.length < 2) {
+        alert("cantidad de participantes inferior al minimo (2 o más para continuar)");
+        return;
+    } {
+        let posicionAleatoria = Math.floor(Math.random()*listaDeAmigos.length);
+        let amigoSorteado = listaDeAmigos[posicionAleatoria];
+        alert(El amigo secreto sorteado es ${amigoSorteado});
+        limpiarLista();
+        return;
+    };
+    }
+
+
 
 
 
