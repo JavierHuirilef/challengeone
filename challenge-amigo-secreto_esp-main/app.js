6cc9861d11904f5d3d1542a1f577201b62553202
Javier <JAVIERHUIRILEF1@GMAIL.COM>
2025-08-10 16:48:24 -0400

    Update app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,12 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo = document.getElementById('amigo');
 let listaDeAmigos = [];
 let ulListaDeAmigos = document.getElementById('listaAmigos');//mostrar lista de amigos en etiqueta ul
 
+
 function agregarAmigo(){
     let nombreAmigo = amigo.value;
     if(nombreAmigo === ''){
-        alert("-debe ingresar un nombre a la lista");
+        alert("debe ingresar un nombre a la lista");
         return;
     }else if(!isNaN(nombreAmigo)){
         alert("El valor ingresado es incorrecto, favor intente de nuevo");
@@ -16,25 +16,27 @@ function agregarAmigo(){
         return;
     } else{
         listaDeAmigos.push(nombreAmigo);//agrega amigo a la lista
-        ulListaDeAmigos.innerHTML = ulListaDeAmigos.innerHTML + <li>${nombreAmigo}</li>;
+        ulListaDeAmigos.innerHTML = ulListaDeAmigos.innerHTML + `<li>${nombreAmigo}</li>`;
         limpiarCampoNombre();
     }   
 };
+
 function sortearAmigo(){
     if(listaDeAmigos.length=== 0){
-        alert("La lista debe contener nombres");
+        alert("La lista no debe estar vacía");
         return;
     }else if(listaDeAmigos.length < 2) {
-        alert("cantidad de participantes inferior al minimo (2 o más para continuar)");
+        alert("antidad de participantes inferior al minimo (2 o más para continuar)");
         return;
     } {
         let posicionAleatoria = Math.floor(Math.random()*listaDeAmigos.length);
         let amigoSorteado = listaDeAmigos[posicionAleatoria];
-        alert(El amigo secreto sorteado es ${amigoSorteado});
+        alert("El amigo secreto sorteado es ${amigoSorteado}");
         limpiarLista();
         return;
     };
     }
+
 function limpiarCampoNombre(){ 
     document.getElementById('amigo').value='';
 }    
@@ -42,9 +44,6 @@ function limpiarLista(){
     listaDeAmigos = []; 
     ulListaDeAmigos.innerHTML = '';
 }
-
-
-
 
 
 
